c1466df7e0f5574346fa3186c14270a0480b36eb
Shravan Rajput <shrrvvuurajput@gmail.com>
2026-02-19 13:35:25 +0530

    done absic layout header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import { AiOutlineSearch } from "react-icons/ai";
+import { MdNotifications, MdApps } from "react-icons/md";
 const Header = () => {
   return (
-    <div className='border border-dark'>
-      header
+    <div className="border border-dark header">
+      <FaBars size={26} className="header__menu" />
+      <img
+        src="https://static.vecteezy.com/system/resources/thumbnails/042/127/234/small/white-square-bordered-youtube-logo-on-transparent-background-free-png.png"
+        alt="youtube logo"
+        className="header__logo "
+      />
+      <form>
+        <input type="text"  placeholder="Search"/>
+        <button type="submit" size={22}><AiOutlineSearch/></button>
+      </form>
+      <div className="header__icons">
+        <MdNotifications size={28}/>
+        <MdApps size={28}/>
+        <img src="https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png" alt="user " className="image" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
