9ba2d0118303194e59bfc2d3cf0be432ff14f0b3
Shravan Rajput <shrrvvuurajput@gmail.com>
2026-02-21 16:38:08 +0530

    video done

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import {AiFillEye} from "react-icons/ai"
+import "./stylesheet/apptwo.css"
 
 const Video = () => {
   return (
-    <div>
-     Video
+    <div className='video'>
+     <div className="video__top">
+      <img src="https://i.ytimg.com/vi/l6E16JAk_Fs/hqdefault.jpg?sqp=-oaymwExCOADEI4CSFryq4qpAyMIARUAAIhCGAHwAQH4Af4JgALQBYoCDAgAEAEYMyBUKHIwDw==&rs=AOn4CLDhGkcKxsuh-ASYBWvyqw2Tvw_2Bg" alt="" />
+      <span>05:43</span>
+     </div>
+     <div className="video__title">
+      Create App in 5 minutes made by chintu
+     </div>
+     <div className="video__detail">
+      <span>
+        <AiFillEye/> 5m views
+      </span>
+      <span>
+        5 days ago
+      </span>
+     </div>
+     <div className="video__channel">
+      <img src="" alt="" />
+      <p>Rainbow head Junior</p>
+     </div>
     </div>
   )
 }
