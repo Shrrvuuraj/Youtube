3576ca345bdcf1a091b00a9a48dc8bad9f63e9c3
Shravan Rajput <shrrvvuurajput@gmail.com>
2026-02-24 15:20:31 +0530

    gooooooooo

---
+++ src/redux/storeSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState={
+     name:"sumit",
+     age:"21"
+}
+const Slice=createSlice({
+     name:"counter",
+     initialState,
+     reducers:{
+
+     }
+})
